5716ccc59691fc7fdacca149353397146f66999c
Yang-JunHo <yjy3070@naver.com>
2025-01-07 22:34:20 +0900

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,12 @@ import Login from './pages/Login';
 import Signin from './pages/Signin';
 import Home from './pages/Home'
 import SearchId from './pages/SearchId';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Header = () => {
+    return (
+        <Navbar expand="lg" className="bg-primary bg-opacity-75">
+            <Container fluid>
+                <Navbar.Brand href="/">Notice Board</Navbar.Brand>
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/signin">Signin</Nav.Link>
+                    </Nav>
+                    {/* <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form> */}
+                    <Form>
+                        <input placeholder='Search'></input>
+                        <Button type='submit'>Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
