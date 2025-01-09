45565c91bafad2d87944bd302c7bd5b5986b2292
Yang-JunHo <yjy3070@naver.com>
2025-01-09 16:45:40 +0900

    signIn

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react'
 import instance from '../axios'
 import { useNavigate } from 'react-router-dom'
+import '../css/Signin.css'
 
 const Signin = () => {
     const [id, setId] = useState()
@@ -25,19 +26,43 @@ const Signin = () => {
         }
     }
     return (
-        <div>
+        <form className='signInForm' onSubmit={sendData}>
             <div>
-                <form onSubmit={sendData}>
-                    <label>ID :</label>
-                    <input type="text" onChange={(e) => { setId(e.target.value) }}/><br />
-                    <label>PW :</label>
-                    <input type='password' onChange={(e) => { setPw(e.target.value) }}/><br />
-                    <label>User :</label>
-                    <input type="text" onChange={(e) => { setUsername(e.target.value) }}/>
-                    <input type="submit"/>
-                </form>
+                <h1 id='register_title'> 회원가입</h1>
             </div>
-        </div>
+            <div className='register'>
+                <div>
+                    {/* 아이디 */}
+                    <div>
+                        <h5> 아이디 </h5>
+                        <input type='text' class="input-field" maxLength='20' onChange={(e) => { setId(e.target.value) }} name='register_id' placeholder="7자 이상의 문자" autoFocus />
+                        <button className='signInButton' type="button" id="dupIdCheck">중복확인</button>
+                    </div>
+
+                    {/* 비밀번호 */}
+                    <div>
+                        <h5> 비밀번호 </h5>
+                        <input type='password' class="input-field" maxLength='15' onChange={(e) => { setPw(e.target.value) }} name='register_password' placeholder="비밀번호" />
+                    </div>
+
+                    {/* 비밀번호 */}
+                    <div>
+                        <h5> 비밀번호 확인 </h5>
+                        <input type='password' class="input-field" maxLength='15' name='register_pswCheck' placeholder="비밀번호 확인" />
+                    </div>
+
+                    {/* 닉네임 */}
+                    <div>
+                        <h5> 닉네임 </h5>
+                        <input type='text' class="input-field" maxLength='10' onChange={(e) => { setUsername(e.target.value) }} name='register_name' placeholder="이름" />
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <button type="submit" className='signInButton' id="sbtn">가입하기&nbsp;🎉</button>
+            </div>
+        </form>
     )
 }
 
